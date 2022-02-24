715a584aaf6abad6e117940b3499ec5d1e1b659a
Colin Bacon <colin.bacon@singtel.com>
2022-02-24 10:45:48 +0800

    storybook configuration for prop types to work

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
 module.exports = {
+  env: {
+    development: {
+      plugins: [
+        ['react-docgen', {DOC_GEN_COLLECTION_NAME: 'STORYBOOK_REACT_CLASSES'}],
+      ],
+    },
+  },
+
   presets: ['module:metro-react-native-babel-preset'],
 };
